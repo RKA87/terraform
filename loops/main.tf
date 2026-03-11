ffb0faf6be7a712627baab74466d99a1e0ea82e1
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-03-11 10:36:47 -0500

    terraform practice using count (for loop) and dynamic block from variables

---
+++ loops/main.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "robboshop" {
+    count = length(var.instances)
+    ami           = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.robboshop.id]
+    tags = merge(var.common_tags, {
+        Name = "${var.instances[count.index]}-dev" #mongodb-dev, redis-dev, mysql-dev, catalogue-dev, cart-dev, user-dev, shipping-dev, payment-dev, rabbitmq-dev, frontend-dev
+    })
+}
+
+resource "aws_security_group" "robboshop" {
+    name        = "robboshop-sg"
+    description = "Security group for Roboshop instances"
+    dynamic "ingress" {
+        for_each = var.ingress_rules
+        content {
+            from_port   = ingress.value.from_port
+            to_port     = ingress.value.to_port
+            protocol    = ingress.value.protocol
+            cidr_blocks = ingress.value.cidr_blocks
+        }
+    }
+    dynamic "egress" {
+        for_each = var.egress_rules
+        content {
+            from_port   = egress.value.from_port
+            to_port     = egress.value.to_port
+            protocol    = egress.value.protocol
+            cidr_blocks = egress.value.cidr_blocks
+        }
+    }
+    tags = var.common_tags
+}
+
+resource "aws_lb_target_group" "roboshop" {
+    name     = "roboshop-tg"
+    port     = 80
+    protocol = "HTTP"
+    vpc_id   = var.vpc_id
+    tags = var.common_tags
+    dynamic "health_check" {
+        for_each = var.healthcheck == null ? [] : [var.healthcheck]
+        content {
+            healthy_threshold   = health_check.value.healthy_threshold
+            unhealthy_threshold = health_check.value.unhealthy_threshold
+            timeout             = health_check.value.timeout
+            interval            = health_check.value.interval
+            path                = health_check.value.path
+            protocol            = health_check.value.protocol
+            matcher             = "200-399"
+        }
+    }
+}
---
+++ loops/variables.tf
@@ -0,0 +1,94 @@
+variable "ami_id" {
+  description = "The ID of the AMI to use for the instance."
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  description = "The type of instance to create."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "common_tags" {
+  description = "Common tags for all resources."
+  type        = map(string)
+  default = {
+    Environment = "dev"
+    Project     = "roboshop"
+    Deployment = "terraform"
+  }
+}
+
+variable "instances"{
+    type = list(string)
+    default = ["mongodb", "redis", "mysql", "catalogue", "cart", "user", "shipping", "payment", "rabbitmq", "frontend", "dispatch"]
+}
+
+variable "ingress_rules"{
+    type = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = list(string)
+    }))
+    default = [
+        {
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    ]
+}
+
+variable "egress_rules"{
+    type = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = list(string)
+    }))
+    default = [
+        {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    ]
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be created."
+  type        = string
+  default     = "vpc-0a1e8f6f70460777f"
+}
+
+
+# Health check configuration for TG is loaded with one set of values without multiple blocks as list and it can be overriden
+variable "healthcheck" {
+  description = "Health check configuration for the instances."
+  type = object({
+    interval            = number
+    timeout             = number
+    healthy_threshold   = number
+    unhealthy_threshold = number
+    path                = string
+    protocol            = string
+  })
+  default = {
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    path                = "/health"
+    protocol            = "HTTP"
+  }
+}
