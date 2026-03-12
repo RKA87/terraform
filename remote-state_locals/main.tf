7ea76ad11dc1ef786a17e5a27bc1cee41444937f
Rakesh Kumar Akuluru <rakesh@Rakeshs-MacBook-Pro.local>
2026-03-11 22:33:59 -0500

    terraform practice use-case is remote state files & locals

--- remote-state_locals/main.tf
+++ remote-state_locals/main.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "roboshop"{
 
 resource "aws_security_group" "roboshop" {
     name = "roboshop-${var.environ}"
-    
-  
+    description = "This security group is for roboshop project"
+    ingress = [
+        {
+            from_port = 22
+            to_port = 22
+            cidr_blocks = ["0.0.0.0/0"]
+            protocol = "tcp"
+        }
+    ]
 }
