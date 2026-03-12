9db67d16394789ad899f512cb824795b5dd9e83b
Rakesh Kumar Akuluru <rakesh@Rakeshs-MacBook-Pro.local>
2026-03-11 21:53:22 -0500

    terraform practice use-case is remote state files & locals

--- remote-state_locals/providers.tf
+++ remote-state_locals/providers.tf
@@ -5,14 +5,11 @@ terraform {
       version = "6.35.1"
     }
   }
-
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "path/to/my/terraform.tfstate"
     region         = "us-east-1"
-    # Optional: Enable state locking with DynamoDB
-    dynamodb_table = "terraform-state-locks"
-    # Optional: Enable server-side encryption
+    use_lockfile   = true
     encrypt        = true
   }
 }
