24d5d615cdd8f5a4a06d4984ff84536bde17ef65
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-03-16 16:01:42 -0500

    local-remote executions using provisioners in terraform

---
+++ local-remote-exec/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  region = var.region
+  instance_type = var.instance_type
+  tags = var.common_tags
+  environment = var.environ
+}
---
+++ local-remote-exec/variables.tf
@@ -0,0 +1,66 @@
+variable "instance_type" {
+  description = "The type of instance to create"
+  default     = "t2.micro"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "environ" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "dev"
+    Project     = "local-remote-exec"
+    Deployment  = "terraform"
+  }
+}
+
+variable "instances" {
+  type    = list(object)
+  default = ["mongodb", "mysqld", "redisdb", "rabbitmq"]
+}
+
+variable "ingress_rule" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "egress_rule" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
